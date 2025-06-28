868bbbc3dccb4ea004eb72cbbfb73658b0694e18
Michał Waniewski <m.waniewski2006@gmail.com>
2025-06-28 21:58:43 +0200

    Fix: use process.env.PORT for Railway

--- server.js
+++ server.js
@@ -2,7 +2,8 @@ const express = require('express');
 const path = require('path');
 const data = require('./data.json');
 const app = express();
-const PORT = 3000;
+const PORT = process.env.PORT || 3000;
+
 
 app.use(express.static(path.join(__dirname, 'public')));
 
